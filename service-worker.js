90377fe2f9f8f82168045e20767f38694bccd6fd
leoklyvert <leomoreira_icm@hotmail.com>
2026-08-23 15:49:08 -0300

    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,45 @@
+# service-worker.js
+
+```javascript
+const CACHE_NAME = "leospotter-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./imagens/logo-leospotter.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARQUIVOS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(chaves => {
+      return Promise.all(
+        chaves
+          .filter(chave => chave !== CACHE_NAME)
+          .map(chave => caches.delete(chave))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resposta => {
+        return resposta || fetch(event.request);
+      })
+  );
+});
+```
 
