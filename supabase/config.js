899f1d68781666d1ddef605fa2ef14ab4621261c
leoklyvert <leomoreira_icm@hotmail.com>
2026-08-23 16:29:12 -0300

    Add Supabase configuration file

---
+++ supabase/config.js
@@ -0,0 +1,10 @@
+// ==========================================
+// LeoSpotter
+// Configuração Supabase
+// ==========================================
+
+const SUPABASE_URL =
+    "https://zidqhbvmiqepalnotias.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_GNVmjjw17YmC31W5MdJboA_oi031p5V";
